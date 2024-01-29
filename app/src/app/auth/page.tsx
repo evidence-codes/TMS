f056a4bf2b2084de14f8ae457c939ae2817bf613
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-29 16:16:55 +0100

    feat: display auth forms

---
+++ app/src/app/auth/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Form from "@/components/Form";
+
+export default function Page() {
+  return (
+    <main className="bg-red-400 h-screen flex justify-center items-center">
+      <div>
+        <Form />
+      </div>
+    </main>
+  );
+}
