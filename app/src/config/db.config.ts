02b6c5f6702eed0413fc47493f18be29ff02e7d6
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-29 19:09:36 +0100

    feat: create connection to database

---
+++ app/src/config/db.config.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const DB_URI = process.env.MONGODB_URI;
+
+async function connectDB() {
+  try {
+    if (!DB_URI) {
+      throw new Error("MONGODB_URI is not defined in the environment");
+    }
+
+    await mongoose.connect(DB_URI);
+
+    // await mongoose.connection.dropDatabase();
+    console.log("Connected to MongoDB!");
+  } catch (error: any) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+
+export { connectDB };
