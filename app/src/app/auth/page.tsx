9c4f6a386b11cf38c33dc1b7544303aa6940bc9b
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-29 17:16:26 +0100

    refactor: move page code to layout

--- app/src/app/auth/page.tsx
+++ app/src/app/auth/page.tsx
@@ -1,12 +1,5 @@
 "use client";
-import Form from "@/components/Form";
 
 export default function Page() {
-  return (
-    <main className="bg-red-400 h-screen flex justify-center items-center">
-      <div>
-        <Form />
-      </div>
-    </main>
-  );
+  return <h1></h1>;
 }
--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Form from "@/components/Form";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="bg-red-400 h-screen flex justify-center items-center">
+          <div>
+            <Form />
+          </div>
+        </main>
+        {children}
+      </body>
     </html>
   );
 }
